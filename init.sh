b1748b353ff58b555d13bc6754f6c69433f03c93
Eung Jin Lee <oiojin831@gmail.com>
2016-09-15 19:46:54 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -46,24 +46,17 @@ echo $PUBLIC_KEY | cat >> .ssh/authorized_keys
 # Zsh
 
 # Install vim plug
-wget -fLo ~/.vim/autoload/plug.vim
-https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wget -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Dotfiles
-wget -O ~/.tmux.conf
-https://raw.githubusercontent.com/christoomey/dotfiles/master/tmux/tmux.conf
-wget -O ~/.vimrc
-https://raw.githubusercontent.com/oiojin831/dotfiles/master/vimrc
-wget -O ~/.vimrc.bundles
-https://raw.githubusercontent.com/oiojin831/dotfiles/master/vimrc.bundles
+wget -O ~/.tmux.conf https://raw.githubusercontent.com/christoomey/dotfiles/master/tmux/tmux.conf
+wget -O ~/.vimrc https://raw.githubusercontent.com/oiojin831/dotfiles/master/vimrc
+wget -O ~/.vimrc.bundles https://raw.githubusercontent.com/oiojin831/dotfiles/master/vimrc.bundles
 
 # oh-my-zsh
-sh -c "$(wget
-https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
--O -)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 # theme
-wget -O ~/.oh-my-zsh/themes/xxf.zsh-theme
-https://gist.githubusercontent.com/oiojin831/646c517ffea9d4c67e1ed2619b99da0d/raw/2b5231ff4722382a98286c127b8fa0dee1c47aa6/xxf.zsh-theme
+wget -O ~/.oh-my-zsh/themes/xxf.zsh-theme https://gist.githubusercontent.com/oiojin831/646c517ffea9d4c67e1ed2619b99da0d/raw/2b5231ff4722382a98286c127b8fa0dee1c47aa6/xxf.zsh-theme
 # change theme
 sed -i "s/robbyrussell/xxf/" .zshrc
 
