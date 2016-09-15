2822dc706c7881085fa0641b3acab9c29149a6f0
Eung Jin Lee <oiojin831@gmail.com>
2016-09-15 21:16:21 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -37,14 +37,14 @@ sudo su - oiojin831 <<'EOF'
 # set ssh
 echo "in oiojin831"
 cd ~
-mkdir /.ssh
+mkdir .ssh
 chmod 700 .ssh
 touch .ssh/authorized_keys
 chmod 600 .ssh/authorized_keys
 PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdod/sf8GyY980SGyvRrzcKVfLSyDcM8bpheuJCa28Xps7WU+FKnxkRXCknC2CPiZnXhJpwbwVG+SHh+VABzYZVAmmkW/FzJUMzdagEEtkXTdpsQBPKfmNf8+kFNDxKSac5FPHz/hInzB4/3avsZviy3tRvk86npF9huvvj31mUk6G2GRk2+bjO0MGCY2RImpmQeTZvE7r81EwoA8EXu66HlWCAmNyFpFwNCzKiLuH6X16m00O96knRXWRmKEo8iTue5hoBNdg01xReiQ2crvobayTFyeiOQcj1nlIDMpRlnw/0SzzZumFu5tSW2VWDQ/zdNEhM9RDVZ5eSLPmLCx9 ipad-blink"
 PUBLIC_KEY2="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCve6vpUfZVgk2UPWKNXYPQ+8Z3Qx5veYziUmbYY223npb5yk04dIqMMUeMQTg51o9DZEy1Jbrklugeyc0R82pwt9u/NuNsFJJuuVFaBrHNSFhZp3wqT0pjobnCFTfBzn66EEnFd2pRiFOuOTAK6LQw5Fmxpms     hrvnUpNOzmlgw81HLSHaBOmXvlksQdII/b7lApovVSH5CIhdAZU6xbB73YsJK+iRIj65feCqdoq2YmAADjPaZj68vU5PsqU0HTkHb4chtzWN8AQKTNMkhltkzqo7FvhxcX1uzmKi3ibRvdHZfjl1jEmQpxNVexSYQoq5WZtddKfm23OpVF+neebXoVLt460FV8Q     Nw5gvwTzNxpYs5xMvmhCT45HsTADbproyRSdkSGzlJ9QdRmRxbnU5bXkmu89LzbfUdSeayaqstsk2jRbyjQoNQ3olUrO1mHzjuDOY8KUVZrzqJhbkmZsU4NckWk3lRzlplfnTlaD47saXf/xW87bAlnUXfP634KzDsNyEz0uTL3c6w+9MRb5NkwTRcvYKMyW5TB     yJYpJVneMVvHKKjGYzZYeOwS+gLxSVE4t3c5uHf29nynIMugK7ah+q7S18S7LsOcijyaOsKY4zCkLw58MVpqESMOBuhwt99GOYh72LJaUr+Sj/YVZjr83refPmXPsEbMhTKUlnGKw== withsellit.com-macbook"
-echo $PUBLIC_KEY | cat >> /home/oiojin831/.ssh/authorized_keys
-echo $PUBLIC_KEY2 | cat >> /home/oiojin831/.ssh/authorized_keys
+echo $PUBLIC_KEY | cat >> .ssh/authorized_keys
+echo $PUBLIC_KEY2 | cat >> .ssh/authorized_keys
 
 # Install vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
